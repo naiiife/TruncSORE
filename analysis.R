dda2dd9cbc2b52b50091fbafda6e2dba0e8ad52a
naiiife <61304007+naiiife@users.noreply.github.com>
2023-10-01 20:09:15 +0800

    Update analysis.R

--- analysis.R
+++ analysis.R
@@ -5,5 +5,5 @@ S = as.numeric(dat$TRM==0|dat$TRMT>730)
 Y = 1-(dat$RELAPSE==0|dat$RELAPSET>730)
 M = dat$MRD
 
-sore(Z, S, Y, V, need.variance=TRUE, nboot=1000)
 sc(Z, S, Y, V, need.variance=TRUE, nboot=1000)
+sore(Z, S, Y, V, need.variance=TRUE, nboot=1000)
