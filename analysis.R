b1eb601078022847d307529e0e1dd37351b67d85
naiiife <61304007+naiiife@users.noreply.github.com>
2023-10-01 20:06:47 +0800

    Create analysis.R

---
+++ analysis.R
@@ -0,0 +1,9 @@
+dat = read.csv('leukemiaPKU.csv')
+Z = 1-dat$TRANSPLANT
+V = as.numeric(dat$AGE>27.15)
+S = as.numeric(dat$TRM==0|dat$TRMT>730)
+Y = 1-(dat$RELAPSE==0|dat$RELAPSET>730)
+M = dat$MRD
+
+sore(Z, S, Y, V, need.variance=TRUE, nboot=1000)
+sc(Z, S, Y, V, need.variance=TRUE, nboot=1000)
